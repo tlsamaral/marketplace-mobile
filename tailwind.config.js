7a3f3862c69a228a89252fdf74c10b8e509435a3
Talles <worktalles@hotmail.com>
2025-12-27 19:48:19 -0300

    chore: import base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const { colors } = require("./src/styles/colors")
+
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors
+    },
   },
   plugins: [],
 }
