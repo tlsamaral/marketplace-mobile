84b3a0a992c3eb1e1baca08cec7de903ceb2f643
Talles <worktalles@hotmail.com>
2025-12-27 19:42:54 -0300

    chore: config nativewindcss

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { Text, TouchableOpacity, View } from "react-native";
 export default function App() {
   return (
     <View>
-      <Text>Ola mundo</Text>
+      <Text>Ola mundo 2</Text>
 
       <TouchableOpacity onPress={() => router.push("/login")}>
         <Text>Entrar</Text>
