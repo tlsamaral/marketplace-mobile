dc7167d6285a050568ae184df4dd367f699f2381
Talles <worktalles@hotmail.com>
2025-12-27 17:24:55 -0300

    setup

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Ola mundo</Text>
+
+      <TouchableOpacity onPress={() => router.push("/login")}>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
