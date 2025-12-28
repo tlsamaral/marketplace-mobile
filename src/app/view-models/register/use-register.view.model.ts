e3d31a02beec8105af08976c4047d13111d767f9
Talles <worktalles@hotmail.com>
2025-12-27 21:59:33 -0300

    chore: create view model bridge

--- src/app/view-models/register/use-register.view.model.ts
+++ src/app/view-models/register/use-register.view.model.ts
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import { type RegisterFormData, registerScheme } from './register.scheme'
+import { useRegisterMutation } from '../../shared/queries/auth/use-register.mutation'
 
 export const useRegisterViewModel = () => {
-  const [userData, setUserData] = useState()
+  const userRegisterMutation = useRegisterMutation()
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>({
+    resolver: yupResolver(registerScheme),
+  })
+
+  const onSubmit = handleSubmit(async (data) => {
+    const { confirmPassword: _, ...registerData } = data
+
+    await userRegisterMutation.mutateAsync(registerData)
+  })
 
   return {
-    userData,
-    setUserData,
+    control,
+    onSubmit,
+    errors,
   }
 }
