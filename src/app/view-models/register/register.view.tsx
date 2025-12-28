e83497530e93c355860bc40fc41d9321d1d7761f
Talles <worktalles@hotmail.com>
2025-12-27 21:59:10 -0300

    chore: create base view

--- src/app/view-models/register/register.view.tsx
+++ src/app/view-models/register/register.view.tsx
@@ -3,8 +3,9 @@ import { Text, View } from 'react-native'
 import type { useRegisterViewModel } from './use-register.view.model'
 
 export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
-  userData,
-  setUserData,
+  control,
+  errors,
+  onSubmit,
 }) => {
   return (
     <View>
