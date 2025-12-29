67ecf5c45fc23e68c5d3fbb2c89ad0c15b8c388e
Talles <worktalles@hotmail.com>
2025-12-29 19:26:22 -0300

    chore: button to register page

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from 'react-native'
+import { router } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 export default function Login() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="">Login</Text>
+
+      <TouchableOpacity onPress={() => router.push('/register')}>
+        <Text>Register</Text>
+      </TouchableOpacity>
     </View>
   )
 }
