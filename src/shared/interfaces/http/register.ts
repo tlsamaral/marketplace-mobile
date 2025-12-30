014ff68bc6a69badacdff55e003eaddb66d92795
Talles <worktalles@hotmail.com>
2025-12-29 21:19:23 -0300

    chore: remove response interface

--- src/shared/interfaces/http/register.ts
+++ src/shared/interfaces/http/register.ts
@@ -1,15 +1,7 @@
-import type { UserInterface } from '../user'
-
 export interface RegisterHttpParams {
   name: string
   email: string
   password: string
   avatarUrl?: string
   phone: string
-}
-
-export interface RegisterHttpResponse {
-  user: UserInterface
-  token: string
-  refreshToken: string
 }
