6cc482600d04c5045523584d92d9379589cb5554
Talles <worktalles@hotmail.com>
2025-12-29 21:18:28 -0300

    feat: implement a view screen to login a user

--- src/view-models/login/login.view.tsx
+++ src/view-models/login/login.view.tsx
@@ -1,18 +1,48 @@
 import { router } from 'expo-router'
+import type { FC } from 'react'
 import { Text, TouchableOpacity, View } from 'react-native'
+import { AppInputController } from '../../shared/components/app-input-controller'
 import { AuthFormHeader } from '../../shared/components/auth-form-header'
+import { KeyboardContainer } from '../../shared/components/keyboard-container'
+import type { useLoginViewModel } from './use-login-view.model'
 
-export const LoginView = () => {
+export const LoginView: FC<ReturnType<typeof useLoginViewModel>> = ({
+  control,
+  onSubmit,
+}) => {
   return (
-    <View className="flex-1 items-center justify-center gap-4 p-4">
-      <AuthFormHeader
-        title="Acesse sua conta"
-        subtitle="Informe seu e-mail e senha para entrar"
-      />
+    <KeyboardContainer>
+      <View className="flex-1 items-center justify-center p-[32px]">
+        <AuthFormHeader
+          title="Acesse sua conta"
+          subtitle="Informe seu e-mail e senha para entrar"
+        />
 
-      <TouchableOpacity onPress={() => router.push('/register')}>
-        <Text>Registro</Text>
-      </TouchableOpacity>
-    </View>
+        <AppInputController
+          control={control}
+          name="email"
+          leftIcon="mail-outline"
+          placeholder="mail@example.com.br"
+          label="E-MAIL"
+        />
+
+        <AppInputController
+          control={control}
+          name="password"
+          leftIcon="lock-closed-outline"
+          label="SENHA"
+          placeholder="Sua senha"
+          secureTextEntry
+        />
+
+        <TouchableOpacity onPress={onSubmit} className="mb-4">
+          <Text>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push('/register')}>
+          <Text>Registro</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardContainer>
   )
 }
