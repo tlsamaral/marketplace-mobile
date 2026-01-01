c5cd9b68c7c83e16c23eff3d4373ffa823c31a05
Talles <worktalles@hotmail.com>
2026-01-01 18:53:00 -0300

    chore: include app button

--- src/view-models/register/register.view.tsx
+++ src/view-models/register/register.view.tsx
@@ -1,6 +1,6 @@
 import { router } from 'expo-router'
 import type { FC } from 'react'
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import { AppButton } from '../../shared/components/app-button'
 import { AppInputController } from '../../shared/components/app-input-controller'
 import { AuthFormHeader } from '../../shared/components/auth-form-header'
