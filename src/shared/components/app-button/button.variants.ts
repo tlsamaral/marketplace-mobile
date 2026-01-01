d820443f8ebe5c529a87b9be00f9d5a59facb2a3
Talles <worktalles@hotmail.com>
2026-01-01 18:42:27 -0300

    feat: create styles for app button

--- src/shared/components/app-button/button.variants.ts
+++ src/shared/components/app-button/button.variants.ts
@@ -1,11 +1,51 @@
 import { tv, type VariantProps } from 'tailwind-variants'
 
+export enum AppButtonVariantsEnum {
+  FILLED = 'filled',
+  OUTLINED = 'outlined',
+}
+
 export const buttonVariants = tv({
   slots: {
     base: 'w-full h-[48px] rounded-[10px] border px-4 flex-row items-center',
+    text: 'text-semibold text-base',
   },
-  variants: {},
-  defaultVariants: {},
+  variants: {
+    hasIcon: {
+      true: {
+        base: 'justify-between',
+      },
+      false: {
+        base: 'justify-center',
+      },
+    },
+    isLoading: {
+      true: {
+        base: 'opacity-50',
+      },
+    },
+    isDisabled: {
+      true: {
+        base: 'opacity-50',
+      },
+    },
+    variant: {
+      filled: {
+        base: 'bg-purple-base border-purple-base',
+        text: 'text-white',
+      },
+      outlined: {
+        base: 'bg-transparent border-purple-base',
+        text: 'text-purple-base',
+      },
+    },
+  },
+  defaultVariants: {
+    hasIcon: false,
+    isLoading: false,
+    isDisabled: false,
+    variant: AppButtonVariantsEnum.FILLED,
+  },
 })
 
 export type ButtonVariants = VariantProps<typeof buttonVariants>
