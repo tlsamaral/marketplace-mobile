3b9a9307b4aec01b8cd309e255de3521dc392f6d
Talles <worktalles@hotmail.com>
2026-01-02 20:48:28 -0300

    chore: create state to get image uri

--- src/view-models/register/use-register.view.model.ts
+++ src/view-models/register/use-register.view.model.ts
@@ -1,38 +1,22 @@
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { useAppModal } from '../../shared/hooks/use-app-modal'
-import { useCamera } from '../../shared/hooks/use-camera'
-import { useGallery } from '../../shared/hooks/use-gallery'
+import { useImage } from '../../shared/hooks/use-image'
 import { useRegisterMutation } from '../../shared/queries/auth/use-register.mutation'
 import { useUserStore } from '../../shared/store/user-store'
 import { type RegisterFormData, registerScheme } from './register.scheme'
 
 export const useRegisterViewModel = () => {
+  const [avatarUri, setAvatarUri] = useState<string | null>(null)
+
   const userRegisterMutation = useRegisterMutation()
   const { setSession } = useUserStore()
-  const { openCamera } = useCamera({})
-  const { openGallery } = useGallery({})
-  const modals = useAppModal()
+  const { handleSelectImage } = useImage({
+    callback: setAvatarUri,
+  })
 
-  const handleSelectAvatar = () => {
-    modals.showSelection({
-      title: 'Selecione um avatar',
-      message: 'Escolha uma opção',
-      options: [
-        {
-          text: 'Galeria',
-          icon: 'images',
-          variant: 'primary',
-          onPress: openGallery,
-        },
-        {
-          text: 'Câmera',
-          icon: 'camera',
-          variant: 'primary',
-          onPress: openCamera,
-        },
-      ],
-    })
+  const handleSelectAvatar = async () => {
+    await handleSelectImage()
   }
 
   const {
