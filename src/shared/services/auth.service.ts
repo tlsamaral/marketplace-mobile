721afbed60cf76a78ba24c2cbd8ba578d96e6d1d
Talles <worktalles@hotmail.com>
2026-01-02 21:14:31 -0300

    chore: create function to do a upload avatar

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,7 +1,8 @@
-import { marketPlaceApiClient } from '../api/market-place'
+import { baseURL, marketPlaceApiClient } from '../api/market-place'
 import type { AuthResponse } from '../interfaces/http/auth-response'
 import type { LoginHttpParams } from '../interfaces/http/login'
 import type { RegisterHttpParams } from '../interfaces/http/register'
+import type { UploadAvatarResponse } from '../interfaces/http/upload-avatar'
 
 export const register = async (userData: RegisterHttpParams) => {
   const { data } = await marketPlaceApiClient.post<AuthResponse>(
@@ -17,6 +18,30 @@ export const login = async (userData: LoginHttpParams) => {
     '/auth/login',
     userData,
   )
+
+  return data
+}
+
+export const uploadAvatar = async (avatarUri: string) => {
+  const formData = new FormData()
+
+  formData.append('avatar', {
+    uri: avatarUri,
+    type: 'image/jpeg',
+    name: 'avatar.jpg',
+  } as unknown as Blob)
+
+  const { data } = await marketPlaceApiClient.post<UploadAvatarResponse>(
+    '/user/avatar',
+    formData,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  )
+
+  data.url = `${baseURL}${data.url}`
 
   return data
 }
