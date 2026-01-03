0625a8b267a7acad67739bd3fa17e85100ee0ade
Talles <worktalles@hotmail.com>
2026-01-02 21:15:03 -0300

    chore: create interceptor to get token from async storage and set in authorization headers

--- src/shared/api/market-place.ts
+++ src/shared/api/market-place.ts
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import axios, { type AxiosInstance } from 'axios'
 import { Platform } from 'react-native'
 
@@ -8,7 +9,7 @@ const getBaseUrl = () => {
   })
 }
 
-const baseURL = getBaseUrl()
+export const baseURL = getBaseUrl()
 
 export class MarketPlaceApiClient {
   private instance: AxiosInstance
@@ -18,10 +19,35 @@ export class MarketPlaceApiClient {
     this.instance = axios.create({
       baseURL,
     })
+
+    this.setupInterceptors()
   }
 
   getInstance() {
     return this.instance
+  }
+
+  private setupInterceptors() {
+    this.instance.interceptors.response.use(
+      async (config) => {
+        const userData = await AsyncStorage.getItem('marketplace-auth')
+        console.log(userData)
+        if (userData) {
+          const {
+            state: { token },
+          } = JSON.parse(userData)
+
+          if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+          }
+        }
+
+        return config
+      },
+      (error) => {
+        return Promise.reject(error)
+      },
+    )
   }
 }
 
