f092f0e98362b31a39d09f157d30baa2143bb7e5
Talles <worktalles@hotmail.com>
2026-01-03 19:20:22 -0300

    chore: get on success function by props

--- src/shared/queries/auth/use-register.mutation.ts
+++ src/shared/queries/auth/use-register.mutation.ts
@@ -1,13 +1,28 @@
 import { useMutation } from '@tanstack/react-query'
+import type { AuthResponse } from '../../interfaces/http/auth-response'
 import type { RegisterHttpParams } from '../../interfaces/http/register'
 import * as authService from '../../services/auth.service'
+import { useUserStore } from '../../store/user-store'
 
-export const useRegisterMutation = () => {
+interface UserRegisterMutationParams {
+  onSuccess?: (authResponse: AuthResponse) => void
+}
+
+export const useRegisterMutation = ({
+  onSuccess,
+}: UserRegisterMutationParams = {}) => {
+  const { setSession } = useUserStore()
   const mutation = useMutation({
     mutationFn: (userData: RegisterHttpParams) =>
       authService.register(userData),
-    onSuccess: (response) => {
-      console.log(response)
+    onSuccess: async (response) => {
+      setSession({
+        user: response.user,
+        token: response.token,
+        refreshToken: response.refreshToken,
+      })
+
+      onSuccess?.(response)
     },
     onError: (error) => {
       console.log(error)
