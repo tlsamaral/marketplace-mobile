aa927c755aabb73a2d727036c138ad2e2ad62914
Talles <worktalles@hotmail.com>
2026-01-03 19:18:57 -0300

    chore: use upload avatar mutation when user is registered with success

--- src/view-models/register/use-register.view.model.ts
+++ src/view-models/register/use-register.view.model.ts
@@ -4,14 +4,14 @@ import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useImage } from '../../shared/hooks/use-image'
 import { useRegisterMutation } from '../../shared/queries/auth/use-register.mutation'
+import { useUploadAvatarMutation } from '../../shared/queries/auth/use-upload-avatar.mutation'
 import { useUserStore } from '../../shared/store/user-store'
 import { type RegisterFormData, registerScheme } from './register.scheme'
 
 export const useRegisterViewModel = () => {
   const [avatarUri, setAvatarUri] = useState<string | null>(null)
 
-  const userRegisterMutation = useRegisterMutation()
-  const { setSession } = useUserStore()
+  const { updateUser } = useUserStore()
   const { handleSelectImage } = useImage({
     callback: setAvatarUri,
     cameraType: CameraType.front,
@@ -29,16 +29,21 @@ export const useRegisterViewModel = () => {
     resolver: yupResolver(registerScheme),
   })
 
+  const uploadAvatarMutation = useUploadAvatarMutation()
+
+  const userRegisterMutation = useRegisterMutation({
+    onSuccess: async () => {
+      if (avatarUri) {
+        const { url } = await uploadAvatarMutation.mutateAsync(avatarUri)
+
+        updateUser({ avatarUrl: url })
+      }
+    },
+  })
+
   const onSubmit = handleSubmit(async (data) => {
     const { confirmPassword: _, ...registerData } = data
-
-    const response = await userRegisterMutation.mutateAsync(registerData)
-
-    setSession({
-      user: response.user,
-      token: response.token,
-      refreshToken: response.refreshToken,
-    })
+    await userRegisterMutation.mutateAsync(registerData)
   })
 
   return {
