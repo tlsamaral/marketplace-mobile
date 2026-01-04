c1d4d99d33b90f8bdc4cb1e5045c065ca7ddadeb
Talles <worktalles@hotmail.com>
2026-01-04 15:43:26 -0300

    chore: create validation to user dont use app without login

--- src/app/(private)/_layout.tsx
+++ src/app/(private)/_layout.tsx
@@ -1,5 +1,10 @@
-import { Stack } from 'expo-router'
+import { Redirect, Stack } from 'expo-router'
+import { useUserStore } from '../../shared/store/user-store'
 
 export default function PrivateLayout() {
+  const { user, token } = useUserStore()
+
+  if (!user || !token) return <Redirect href="/(public)/login" />
+
   return <Stack />
 }
