c1396b9d1502e11f4592ff60253e26f799e00695
Talles <worktalles@hotmail.com>
2026-01-04 20:00:44 -0300

    chore: create use query for list infinite products

---
+++ src/shared/queries/product/use-product-infinite-query.ts
@@ -0,0 +1,51 @@
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { buildImageUrl } from '../../helpers/build-image-url'
+import { getProducts } from '../../services/product.service'
+
+export const useProductInfiniteQuery = () => {
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useInfiniteQuery({
+    queryKey: ['products'],
+    queryFn: async ({ pageParam = 1 }) => {
+      try {
+        const response = await getProducts({
+          pagination: { page: pageParam, perPage: 10 },
+        })
+
+        return response
+      } catch (error) {
+        throw error
+      }
+    },
+    getNextPageParam: (last) => {
+      return last.page < last.totalPages ? last.page + 1 : undefined
+    },
+    initialPageParam: 1,
+  })
+
+  const products = data?.pages
+    .flatMap((page) => page.data)
+    .map((product) => ({
+      ...product,
+      photo: buildImageUrl(product.photo),
+    }))
+
+  return {
+    products,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    refetch,
+    isRefetching,
+  }
+}
