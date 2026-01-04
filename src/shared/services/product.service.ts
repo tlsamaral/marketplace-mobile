633db3db776f673ac41e6696457f3dac20d2ef85
Talles <worktalles@hotmail.com>
2026-01-04 17:34:05 -0300

    chore: remove interface from servie

--- src/shared/services/product.service.ts
+++ src/shared/services/product.service.ts
@@ -1,23 +1,6 @@
 import { marketPlaceApiClient } from '../api/market-place'
+import type { ProductRequest } from '../interfaces/http/product'
 import type { ProductResponse } from '../interfaces/http/product-response'
-
-interface ProductRequest {
-  pagination: {
-    page: number
-    perPage: number
-  }
-  filters: {
-    from: Date
-    to: Date
-    categoryIds: number[]
-    searchText: string
-    minValue: number
-    maxValue: number
-  }
-  sort: {
-    averageRating: string
-  }
-}
 
 export const getProducts = async (params: ProductRequest) => {
   const { data } = await marketPlaceApiClient.post<ProductResponse>(
