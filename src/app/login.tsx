a47d2496c8ec0270a80ff2d2d238e4f4a1ab091a
Talles <worktalles@hotmail.com>
2026-01-04 15:44:08 -0300

    deleted

--- src/app/login.tsx
+++
@@ -1,8 +0,0 @@
-import { LoginView } from '../view-models/login/login.view'
-import { useLoginViewModel } from '../view-models/login/use-login-view.model'
-
-export default function Login() {
-  const props = useLoginViewModel()
-
-  return <LoginView {...props} />
-}
--- src/app/register.tsx
+++
@@ -1,8 +0,0 @@
-import { RegisterView } from '../view-models/register/register.view'
-import { useRegisterViewModel } from '../view-models/register/use-register.view.model'
-
-export default function Register() {
-  const props = useRegisterViewModel()
-
-  return <RegisterView {...props} />
-}
