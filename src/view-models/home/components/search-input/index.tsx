ed3a4867a37050a459b3fb85917b887598366c94
Talles <worktalles@hotmail.com>
2026-01-04 16:45:17 -0300

    feat: create uninteractable search input

---
+++ src/view-models/home/components/search-input/index.tsx
@@ -0,0 +1,25 @@
+import { Ionicons } from '@expo/vector-icons'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { AppInput } from '../../../../shared/components/app-input'
+import { colors } from '../../../../styles/colors'
+
+export function SearchInput() {
+  return (
+    <View className="mb-3 mt-6">
+      <Text className="text-2xl font-bold mt-6 mb-2">Explore produtos</Text>
+      <View className="flex-row items-center gap-4">
+        <View className="flex-1">
+          <AppInput leftIcon="search" className="text-lg" />
+        </View>
+
+        <TouchableOpacity className="ml-5 mt-5 items-center justify-center rounded-xl border-[1px] size-[48px] border-purple-base">
+          <Ionicons
+            name="filter-outline"
+            size={24}
+            color={colors['purple-base']}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
