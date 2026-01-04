79656bd70f4855fe5b9f2c18ed8b49b9deaad3a4
Talles <worktalles@hotmail.com>
2026-01-04 20:00:22 -0300

    feat: create home view model

---
+++ src/view-models/home/use-home.view-model.ts
@@ -0,0 +1,26 @@
+import { useProductInfiniteQuery } from '../../shared/queries/product/use-product-infinite-query'
+
+export const useHomeViewModel = () => {
+  const {
+    products,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useProductInfiniteQuery()
+
+  const handleLoadMore = async () => {
+    if (hasNextPage && !isFetchingNextPage && !isLoading) {
+      fetchNextPage()
+    }
+  }
+
+  const handleRefresh = async () => {
+    await refetch()
+  }
+
+  return { handleLoadMore, handleRefresh, products }
+}
