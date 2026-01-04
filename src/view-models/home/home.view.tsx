4ff4d165eb1dcfee96a8cd0e73b0ae689716ac61
Talles <worktalles@hotmail.com>
2026-01-04 17:33:03 -0300

    chore: use product card as a render item and mock products

--- src/view-models/home/home.view.tsx
+++ src/view-models/home/home.view.tsx
@@ -1,14 +1,41 @@
 import { FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import type { ProductInterface } from '../../shared/interfaces/product'
 import { HomeHeader } from './components/header'
+import { ProductCard } from './components/product-card'
 import { SearchInput } from './components/search-input'
 
 export const HomeView = () => {
+  const productsList: ProductInterface[] = [
+    {
+      id: 0,
+      value: 'string',
+      name: 'string',
+      description: 'string',
+      photo: 'string',
+      height: 'string',
+      width: 'string',
+      weight: 'string',
+      averageRating: 0,
+      views: 0,
+      ratingCount: 0,
+      categoryId: 0,
+      category: {
+        id: 0,
+        name: 'string',
+      },
+      createdAt: 'string',
+      updatedAt: 'string',
+      deletedAt: 'string',
+    },
+  ]
+
   return (
     <SafeAreaView edges={['top']} className="flex-1">
       <FlatList
-        data={[]}
-        renderItem={() => null}
+        data={productsList}
+        renderItem={({ item }) => <ProductCard product={item} />}
+        keyExtractor={(item) => `product-list-item-${item.id}`}
         ListHeaderComponent={() => (
           <>
             <HomeHeader />
