aba8a885f0e45436acef6cffa7b6154f970451b4
Talles <worktalles@hotmail.com>
2026-01-04 15:42:19 -0300

    chore: create public section to wrapper public routes

---
+++ src/app/(public)/login.tsx
@@ -0,0 +1,8 @@
+import { LoginView } from '../../view-models/login/login.view'
+import { useLoginViewModel } from '../../view-models/login/use-login-view.model'
+
+export default function Login() {
+  const props = useLoginViewModel()
+
+  return <LoginView {...props} />
+}
