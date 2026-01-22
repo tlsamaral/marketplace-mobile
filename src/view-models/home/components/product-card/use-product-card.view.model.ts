c8a6d83e9ae5ef05e1780be71eaad09ede006b14
Talles <worktalles@hotmail.com>
2026-01-22 19:54:37 -0300

    chore: format values and export to view

--- src/view-models/home/components/product-card/use-product-card.view.model.ts
+++ src/view-models/home/components/product-card/use-product-card.view.model.ts
@@ -7,5 +7,17 @@ interface UseProductCardViewModelProps {
 export const useProductCardViewModel = ({
   product,
 }: UseProductCardViewModelProps) => {
-  return { product }
+  const formatProductName = (name: string) => {
+    if (name.length >= 17) {
+      return `${name.slice(0, 17)}...`
+    }
+
+    return name
+  }
+
+  const formatRating = product.averageRating.toFixed(1)
+
+  const displayName = formatProductName(product.name)
+
+  return { product, displayName, formatRating }
 }
